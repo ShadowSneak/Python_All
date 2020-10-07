7198b3ae9a243c71557dae28ec5930f8b5bc7b44
ShadowSneak <63430114+ShadowSneak@users.noreply.github.com>
2020-10-07 12:59:50 -0300

    Add files via upload

---
+++ InputComIf.py
@@ -0,0 +1,9 @@
+
+
+num1 = input ('Dê um número: ')
+num2 = input ('Dê um segundo número: ')
+
+if num1 > num2: 
+    print ('O primeiro número é maior!')
+else:
+    print ('O segundo número é maior!')
